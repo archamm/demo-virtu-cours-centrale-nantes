8486d5a95a5b3cf04fae280f248248123847c17c
archamm <matthieu.archambault@epita.fr>
2023-03-16 22:57:01 +0100

    fix detect

--- detect_virtualization.py
+++ detect_virtualization.py
@@ -7,7 +7,7 @@ def detect_virtualization():
     machine = platform.machine()
     processor = platform.processor()
     
-    if "Microsoft" in platform.platform() or "vbox" in platform.platform().lower() or "xen" in platform.platform().lower() or "kvm" in platform.platform().lower():
+    if "Debian" in platform.platform() or "vbox" in platform.platform().lower() or "xen" in platform.platform().lower() or "kvm" in platform.platform().lower():
         virtualization = "Yes"
     else:
         virtualization = "No"
