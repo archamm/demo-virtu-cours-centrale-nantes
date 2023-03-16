2b3ab9c958b8d0f7021369476487046208231b6e
archamm <matthieu.archambault@epita.fr>
2023-03-16 17:03:19 +0100

    Add detect_virtulization script

---
+++ detect_virtualization.py
@@ -0,0 +1,23 @@
+import platform
+
+def detect_virtualization():
+    system = platform.system()
+    release = platform.release()
+    version = platform.version()
+    machine = platform.machine()
+    processor = platform.processor()
+    
+    if "Microsoft" in platform.platform() or "vbox" in platform.platform().lower() or "xen" in platform.platform().lower() or "kvm" in platform.platform().lower():
+        virtualization = "Yes"
+    else:
+        virtualization = "No"
+
+    print(f"System: {system}")
+    print(f"Release: {release}")
+    print(f"Version: {version}")
+    print(f"Machine: {machine}")
+    print(f"Processor: {processor}")
+    print(f"Is it running in a virtual environment? {virtualization}")
+
+if __name__ == "__main__":
+    detect_virtualization()
